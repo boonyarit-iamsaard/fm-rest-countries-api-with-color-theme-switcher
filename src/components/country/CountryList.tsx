9152de06aa2aef0ee2b235f46797e63dab9cd6af
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2022-09-15 18:19:57 +0700

    build: implement scroll to top button

--- src/components/country/CountryList.tsx
+++ src/components/country/CountryList.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, Fragment, useEffect, useState } from 'react';
 
 import { Country } from '@/lib/types';
 import CountryListItem from './CountryListItem';
@@ -20,20 +20,25 @@ const CountryList: FC<CountryListProps> = ({ countryList }) => {
       setPageEnd(pageEnd + pageSize);
     }
   };
+  const handleScrolltoTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
 
   useEffect(() => {
     setPageEnd(pageSize);
   }, [countryList]);
 
   return (
-    <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      {countryList.slice(pageStart, pageEnd).map((country, index) => (
-        <CountryListItem
-          key={country.cca3}
-          country={country}
-          priority={index < 4}
-        />
-      ))}
+    <Fragment>
+      <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {countryList.slice(pageStart, pageEnd).map((country, index) => (
+          <CountryListItem
+            key={country.cca3}
+            country={country}
+            priority={index < 4}
+          />
+        ))}
+      </div>
       {hasNextPage && (
         <button
           className="w-full rounded-lg px-4 py-2 text-center hover:bg-blue-500 hover:text-white"
@@ -42,7 +47,13 @@ const CountryList: FC<CountryListProps> = ({ countryList }) => {
           Load More...
         </button>
       )}
-    </div>
+      <button
+        className="w-full rounded-lg px-4 py-2 text-center hover:bg-blue-500 hover:text-white"
+        onClick={handleScrolltoTop}
+      >
+        Scroll to Top
+      </button>
+    </Fragment>
   );
 };
 
