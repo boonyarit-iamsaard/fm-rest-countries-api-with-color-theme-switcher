0705f325c6292336839ad9594993ef6a1ab10066
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2022-09-16 13:16:19 +0700

    build: implement theme switcher

--- src/components/layout/LayoutHeader.tsx
+++ src/components/layout/LayoutHeader.tsx
@@ -1,14 +1,33 @@
 import { FC } from 'react';
 
+import { MoonIcon, SunIcon } from '@heroicons/react/24/solid';
+import { useTheme } from 'next-themes';
 import Link from 'next/link';
 
 const LayoutHeader: FC = () => {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === 'system' ? systemTheme : theme;
   return (
-    <header className="flex items-center bg-white text-sm shadow-sm">
-      <div className="container mx-auto px-4">
+    <header className="flex items-center bg-white text-sm shadow-sm dark:bg-theme-blue-700">
+      <div className="container mx-auto flex items-center justify-between px-4">
         <Link href="/" className="font-semibold">
           <a className="font-bold">Where in the world?</a>
         </Link>
+        <div>
+          {currentTheme === 'dark' ? (
+            <MoonIcon
+              role="button"
+              className="h-6 w-6 text-white"
+              onClick={() => setTheme('light')}
+            />
+          ) : (
+            <SunIcon
+              role="button"
+              className="h-6 w-6 text-theme-blue-900"
+              onClick={() => setTheme('dark')}
+            />
+          )}
+        </div>
       </div>
     </header>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import { Fragment } from 'react';
 
+import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
-
-import '@/styles/globals.css';
 import Head from 'next/head';
 
 import { Layout } from '@/components/layout';
 import { CountryCodeProvider } from '@/context';
+
+import '@/styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -14,11 +15,13 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <Layout>
-        <CountryCodeProvider>
-          <Component {...pageProps} />
-        </CountryCodeProvider>
-      </Layout>
+      <ThemeProvider attribute="class">
+        <Layout>
+          <CountryCodeProvider>
+            <Component {...pageProps} />
+          </CountryCodeProvider>
+        </Layout>
+      </ThemeProvider>
     </Fragment>
   );
 }
